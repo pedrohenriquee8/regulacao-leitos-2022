3161b40f29b083a1f2bd623a12c6503023c64955
pedrovnogueiras <99842415+pedrovnogueiras@users.noreply.github.com>
2022-06-15 16:47:36 -0300

    Inserção dos primeiros arquivos.

--- projeto_leitos_2022/lib/main.dart
+++ projeto_leitos_2022/lib/main.dart
@@ -1,10 +1,10 @@
-import 'package:projeto_leitos_2022/home_page.dart';
+import 'package:projeto_leitos_2022/tela_login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: HomePage(),
+      home: TelaLogin(),
     ),
   );
 }
