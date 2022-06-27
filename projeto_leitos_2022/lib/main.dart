722a7dd48a6d531afe86118485bd9031452b3f63
pedrovnogueiras <99842415+pedrovnogueiras@users.noreply.github.com>
2022-06-26 23:04:39 -0300

    Add files via upload

---
+++ projeto_leitos_2022/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:projetoleitos/main.dart';
+import 'package:projetoleitos/telacadastro.dart';
+
+void main(){
+  runApp(
+    const MaterialApp(
+    home:HomePage(),
+    ),
+  );
+}
