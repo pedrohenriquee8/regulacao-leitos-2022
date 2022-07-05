e4f42218252d2858339732bc9a78dbadfb1985c5
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2022-07-04 22:36:00 -0300

    Inserção dos arquivos para a página Home.

--- projeto_leitos_2022/lib/main.dart
+++ projeto_leitos_2022/lib/main.dart
@@ -1,10 +1,11 @@
-import 'package:projeto_leitos_2022/home_page.dart';
+import 'package:projeto_leitos_2022/menu.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      home: Menu(),
     ),
   );
 }
